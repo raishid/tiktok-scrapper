bd1eeb2615a936e3cfc1301eeadbf054fd8153f1
fededav <67575679+raishid@users.noreply.github.com>
2023-09-21 17:22:07 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { TTScraper } from "tiktok-scraper-ts";
+
+const app = express();
+app.use(cors());
+
+app.get('/api/profile/tiktok', (req, res) => {
+    console.log(res);
+    
+    /* const scraper = new TTScraper()
+    scraper.user() */
+    res.send('Hello World!')
+})
+
+
+app.listen(3050, () => {});
