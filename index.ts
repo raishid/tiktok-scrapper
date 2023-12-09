b2db90814949e0e125224b4871c4c9d3e23375e6
fededav <67575679+raishid@users.noreply.github.com>
2023-12-09 13:24:52 -0400

    update

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { TTScraper } from "tiktok-scraper-ts";
 import fs from 'fs';
+import 'dotenv/config'
 
 const app = express();
 app.use(cors());
@@ -17,9 +18,24 @@ app.get('/api/profile/tiktok', async (req, res) => {
     const username = query as string
     //verify exist file cookies.txt
     let cookiefile = undefined
-    if(fs.existsSync('./cookies.txt')){
+    
+    const r = await fetch(`${process.env.API_URL}/socials/accounts/tiktok`, {
+        //@ts-ignore
+        headers:{
+            "X-Encryption-Key": process.env.APP_KEY,
+        }
+    })
+
+    const accounts = await r.json()
+    //select random account
+    const account = accounts[Math.floor(Math.random() * accounts.length)];
+
+    if(account.cookies)
+    {
+        fs.writeFileSync('./cookies.txt', account.cookies)
         cookiefile = './cookies.txt'
     }
+    
     const scraper = new TTScraper(cookiefile)
     const data = await scraper.user(username);
     res.send(data)
