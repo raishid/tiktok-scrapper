2f627759d9596e82cfc11e2fba4a6d3be923e29e
fededav <67575679+raishid@users.noreply.github.com>
2023-12-09 14:40:52 -0400

    Revert "update"
    This reverts commit b2db90814949e0e125224b4871c4c9d3e23375e6.

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import { TTScraper } from "tiktok-scraper-ts";
 import fs from 'fs';
-import 'dotenv/config'
 
 const app = express();
 app.use(cors());
@@ -18,24 +17,9 @@ app.get('/api/profile/tiktok', async (req, res) => {
     const username = query as string
     //verify exist file cookies.txt
     let cookiefile = undefined
-    
-    const r = await fetch(`${process.env.API_URL}/socials/accounts/tiktok`, {
-        //@ts-ignore
-        headers:{
-            "X-Encryption-Key": process.env.APP_KEY,
-        }
-    })
-
-    const accounts = await r.json()
-    //select random account
-    const account = accounts[Math.floor(Math.random() * accounts.length)];
-
-    if(account.cookies)
-    {
-        fs.writeFileSync('./cookies.txt', account.cookies)
+    if(fs.existsSync('./cookies.txt')){
         cookiefile = './cookies.txt'
     }
-    
     const scraper = new TTScraper(cookiefile)
     const data = await scraper.user(username);
     res.send(data)
